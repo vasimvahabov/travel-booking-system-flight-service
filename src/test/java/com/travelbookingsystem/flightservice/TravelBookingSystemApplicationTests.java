1ce4a945c837c6222e1d76f005cbd14c3066ee8e
Vasim <vahabov.vasim@gmail.com>
2025-07-11 08:35:04 +0200

    initial commit

---
+++ src/test/java/com/travelbookingsystem/flightservice/TravelBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbookingsystem.flightservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
