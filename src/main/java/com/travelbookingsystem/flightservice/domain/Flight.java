390438133ac20409531580cbc17a6edade740ad6
Vasim <vahabov.vasim@gmail.com>
2025-07-15 03:36:22 +0200

    refactor : Flight model

--- src/main/java/com/travelbookingsystem/flightservice/domain/Flight.java
+++ src/main/java/com/travelbookingsystem/flightservice/domain/Flight.java
@@ -15,35 +15,36 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Flight {
 
+    @NotNull(message = "The airplane id cannot be null!")
     Long airplaneId;
 
     @Pattern(
-            regexp = "^[A-Z]{3}$\n",
+            regexp = "^[A-Z]{2}\\d{1,4}$",
             message = "The flight number is not valid!"
     )
     @NotBlank(message = "The flight number cannot be blank!")
     String number;
 
     @Pattern(
-            regexp = "^[A-Z]{2}\\d{1,4}$\n",
-            message = "The departure airport code number is not valid!"
+            regexp = "^[A-Z]{3}$",
+            message = "The departure airport code number must be 3 uppercase letters (IATA format)!"
     )
     @NotBlank(message = "The departure airport code title cannot be blank!")
     String departureAirportCode;
 
     @Pattern(
-            regexp = "^[A-Z]{2}\\d{1,4}$\n",
-            message = "The arrival airport code number is not valid!"
+            regexp = "^[A-Z]{3}$",
+            message = "The arrival airport code must be 3 uppercase letters (IATA format)!"
     )
     @NotBlank(message = "The arrival airport code title cannot be blank!")
     String arrivalAirportCode;
 
-    @NotBlank(message = "The arrival date time cannot be blank!")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "The arrival date time cannot be blank!")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime arrivalDateTime;
 
-    @NotBlank(message = "The departure date time cannot be blank!")
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @NotNull(message = "The departure date time cannot be blank!")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
     LocalDateTime departureDateTime;
 
     @NotNull(message = "The flight price cannot be null!")
