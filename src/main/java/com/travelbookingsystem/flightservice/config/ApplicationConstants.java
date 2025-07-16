9d1668c6af41b55c79ad323c72be8d1f70365d1a
Vasim <vahabov.vasim@gmail.com>
2025-07-16 04:24:57 +0200

    feat : add ApplicationConstants

---
+++ src/main/java/com/travelbookingsystem/flightservice/config/ApplicationConstants.java
@@ -0,0 +1,11 @@
+package com.travelbookingsystem.flightservice.config;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class ApplicationConstants {
+
+    public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm";
+
+}
