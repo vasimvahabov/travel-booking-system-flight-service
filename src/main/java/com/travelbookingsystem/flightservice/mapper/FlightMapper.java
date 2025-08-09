012eeb78b188c4e871330f76c8e3fb7bcbaa6679
Vasim <vahabov.vasim@gmail.com>
2025-08-09 23:44:00 +0200

    refactor : fix unmapping fields warning on build

--- src/main/java/com/travelbookingsystem/flightservice/mapper/FlightMapper.java
+++ src/main/java/com/travelbookingsystem/flightservice/mapper/FlightMapper.java
@@ -3,17 +3,21 @@ package com.travelbookingsystem.flightservice.mapper;
 import com.travelbookingsystem.flightservice.dtos.request.FlightRequest;
 import com.travelbookingsystem.flightservice.dtos.response.FlightResponse;
 import com.travelbookingsystem.flightservice.entities.Flight;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingConstants;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface FlightMapper {
 
     FlightResponse entityToResponse(Flight flight);
 
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdDateTime", ignore = true)
+    @Mapping(target = "lastModifiedDateTime", ignore = true)
     Flight requestToEntity(FlightRequest flightRequest);
 
+    @Mapping(target = "version", ignore = true)
+    @Mapping(target = "createdDateTime", ignore = true)
+    @Mapping(target = "lastModifiedDateTime", ignore = true)
     Flight updateEntityFromRequest(FlightRequest request, @MappingTarget Flight flight);
 
 }
