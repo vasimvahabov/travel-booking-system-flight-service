fdc850c70909d81142a089134dbe8ceabe4409b1
Vasim <vahabov.vasim@gmail.com>
2025-10-26 00:01:45 +0200

    refactor: adjust jpa tests for auditing

--- src/test/java/com/travelbookingsystem/flightservice/repository/FlightRepositoryJpaTests.java
+++ src/test/java/com/travelbookingsystem/flightservice/repository/FlightRepositoryJpaTests.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -25,17 +27,19 @@ import static com.travelbookingsystem.flightservice.config.ApplicationConstants.
 class FlightRepositoryJpaTests {
 
     final DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
+    final String USER_ID = "182a5a6e-918b-4110-ba0c-8300f6e90662";
 
     @Autowired
     FlightRepository flightRepository;
 
     @Test
-    void findFlightByIdAfterInsert() {
+    @WithMockUser(USER_ID)
+    void whenUnauthenticated_thenThrowsDataIntegrityViolationException() {
         var flight = Flight.builder()
                 .airplaneId(5L)
                 .number("BC544")
                 .departureAirportCode("PLN")
-                    .arrivalAirportCode("GYD")
+                .arrivalAirportCode("GYD")
                 .departureDateTime(LocalDateTime.parse("2025-07-15 00:12", formatter))
                 .arrivalDateTime(LocalDateTime.parse("2025-07-15 05:00", formatter))
                 .price(BigDecimal.valueOf(15.99)).build();
@@ -45,6 +49,21 @@ class FlightRepositoryJpaTests {
         Assertions.assertThat(optionalFlight).isPresent();
         Assertions.assertThat(optionalFlight.get().getNumber()).isEqualTo(flight.getNumber());
 
+    }
+
+    @Test
+    void whenAuthenticated_thenSavesFlight() {
+        var flight = Flight.builder()
+                .airplaneId(5L)
+                .number("BC544")
+                .departureAirportCode("PLN")
+                .arrivalAirportCode("GYD")
+                .departureDateTime(LocalDateTime.parse("2025-07-15 00:12", formatter))
+                .arrivalDateTime(LocalDateTime.parse("2025-07-15 05:00", formatter))
+                .price(BigDecimal.valueOf(15.99)).build();
+        Assertions.assertThatThrownBy(() ->
+                flightRepository.saveAndFlush(flight)
+        ).isInstanceOf(DataIntegrityViolationException.class);
     }
 
 }
