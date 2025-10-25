e4627e7fd2d712ef4bf24f5e705dfb16f837f1e9
Vasim <vahabov.vasim@gmail.com>
2025-10-26 00:56:29 +0200

    chore: update root path in app controller

--- src/main/java/com/travelbookingsystem/flightservice/controller/AppInfoController.java
+++ src/main/java/com/travelbookingsystem/flightservice/controller/AppInfoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/app")
+@RequestMapping("/app")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AppInfoController {
 
