463c0f2a0983f404a9bf8855457c167ce32ab516
Vasim <vahabov.vasim@gmail.com>
2025-11-01 01:10:32 +0100

    chore: add sample logging to AppInfoController

--- src/main/java/com/travelbookingsystem/flightservice/controller/AppInfoController.java
+++ src/main/java/com/travelbookingsystem/flightservice/controller/AppInfoController.java
@@ -4,6 +4,8 @@ import com.travelbookingsystem.flightservice.config.AppInfoProperties;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AppInfoController {
 
+    static final Logger log = LoggerFactory.getLogger(AppInfoController.class);
+
     AppInfoProperties appInfoProperties;
 
     @GetMapping("/info")
     public ResponseEntity<String> info() {
+        log.info("Fetching app info ...");
         return ResponseEntity.ok(String.format("%s %s in %s environment", appInfoProperties.getName(), appInfoProperties.getVersion(), appInfoProperties.getEnvironment()));
     }
 
